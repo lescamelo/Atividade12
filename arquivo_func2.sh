c26db4acd10fd1543787bf0ecfac156428a83514
Valesca Melo <valesca.melo@academico.ifpb.edu.br>
2021-05-26 23:40:03 -0300

    Alterando alguns testes para remover TR, remover SED, substituir AWK

--- arquivo_func2.sh
+++ arquivo_func2.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 function removendo_tr {
-	$(tr -d '[A-Z]' < $1 > /tmp/tr.txt)
-	echo "/tmp/tr.txt"
+	$(tr -d '[A-Z]' < $1 > test)
+	echo "test"
 
 }
 
 
 function removendo_sed {
-	$(sed 's/[0-9]//g' < $1 > /tmp/sed.txt)
-	echo "/tmp/sed.txt"
+	$(sed 's/[0-9]//g' < $1 > test)
+	echo "test"
 }
 
 
 function substituindo_awk {
-	$(awk '{gsub(/[!@#$%¨&\ *\.\,\/\{\[\]\(\)\}]+/, "<?>" )}{print $0}' $1 > /tmp/sawk.txt)
-	echo "/tmp/sawk.txt"
+	$(awk '{gsub(/[!@#$%¨&\ *\.\,\/\{\[\]\(\)\}]+/, "<?>" )}{print $0}' $1 > test)
+	echo "test"
 }
 
 
